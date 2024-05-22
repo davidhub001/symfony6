d4c59b4b511fb129659a70b8800a864fa1e5b0d5
david <david.propitech@gmail.com>
2024-05-22 15:50:58 +0300

    master

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,6 +16,6 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerY5cuxkW\App_KernelDevDebugContainer([
     'container.build_hash' => 'Y5cuxkW',
-    'container.build_id' => '8f93380d',
-    'container.build_time' => 1716377624,
+    'container.build_id' => '4cf54039',
+    'container.build_time' => 1716382253,
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerY5cuxkW');
--- var/cache/dev/url_generating_routes.php
+++ var/cache/dev/url_generating_routes.php
@@ -5,5 +5,6 @@
 return [
     '_preview_error' => [['code', '_format'], ['_controller' => 'error_controller::preview', '_format' => 'html'], ['code' => '\\d+'], [['variable', '.', '[^/]++', '_format', true], ['variable', '/', '\\d+', 'code', true], ['text', '/_error']], [], [], []],
     'front_index' => [[], ['_controller' => 'App\\Controller\\FrontController::index'], [], [['text', '/']], [], [], []],
+    'accueil_index' => [[], ['_controller' => 'App\\Controller\\FrontController::index'], [], [['text', '/accueil']], [], [], []],
     'contact' => [[], ['_controller' => 'App\\Controller\\FrontController::contact'], [], [['text', '/contact']], [], [], []],
 ];
