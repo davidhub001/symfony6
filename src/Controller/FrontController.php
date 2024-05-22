7dc22c42f8eb9c0083cf15300a5f854b2b38bd31
david <david.propitech@gmail.com>
2024-05-22 09:24:15 +0300

    master

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -3,17 +3,16 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FrontController extends AbstractController
 {
     //#[Route('/front', name: 'app_front')]
-    public function index(): JsonResponse
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/FrontController.php',
+        return $this->render('front/index.html.twig', [
+            'controller_name' => 'FrontController',
         ]);
     }
 }
