c6bcb45773662ef0649da772f991398a84a52a14
david <david.propitech@gmail.com>
2024-05-27 11:48:21 +0300

    master

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,22 @@ $(document).ready(function() {
             console.log("Toggle completed");
         });
     });
+    
 });
+
+function scrollGallery(direction) {
+    const galleryRow = document.querySelector('.gallery-row');
+    const scrollAmount = galleryRow.clientWidth;
+    galleryRow.scrollBy({ left: direction * scrollAmount, behavior: 'smooth' });
+}
+
+function autoScroll() {
+    const galleryRow = document.querySelector('.gallery-row');
+    if (galleryRow.scrollLeft + galleryRow.clientWidth >= galleryRow.scrollWidth) {
+        galleryRow.scrollTo({ left: 0, behavior: 'smooth' });
+    } else {
+        galleryRow.scrollBy({ left: galleryRow.clientWidth, behavior: 'smooth' });
+    }
+}
+
+setInterval(autoScroll, 3000); // Scrolls every 3 seconds
