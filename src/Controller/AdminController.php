fa07ad8c8a4687583cc74cee35649fdc2c77ffe4
david <david.propitech@gmail.com>
2024-05-28 10:13:42 +0300

    master

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,14 +5,35 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Form\LoginFormType;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'app_admin')]
-    public function index(): Response
+    public function index(Request $request, SessionInterface $session): Response
     {
+        $form = $this->createForm(LoginFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            if($data['username'] == 'admin' && md5($data['password']) == md5('david123+')){
+                $session->set('user', [
+                    'username' => $data['username'],
+                    'password' => $data['password']
+                ]);
+                return $this->redirectToRoute('dashbord');
+            }
+            return $this->redirectToRoute('admin');
+        }
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'form' => $form->createView()
         ]);
+    }
+    public function dashbord(): Response
+    {
+        return $this->render('admin/dashbord.html.twig');
     }
 }
